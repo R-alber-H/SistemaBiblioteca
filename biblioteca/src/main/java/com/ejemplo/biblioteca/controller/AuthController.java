11e821aed900f7abc64828d3938636a31831aba2
Carlos Basurto Meza <67793086+carlosxd753@users.noreply.github.com>
2025-12-05 02:41:07 -0500

    Revert "Revert "Se implemento jwt""

--- biblioteca/src/main/java/com/ejemplo/biblioteca/controller/AuthController.java
+++ biblioteca/src/main/java/com/ejemplo/biblioteca/controller/AuthController.java
@@ -1,14 +1,20 @@
 package com.ejemplo.biblioteca.controller;
 
+import com.ejemplo.biblioteca.dto.*;
+import com.ejemplo.biblioteca.service.CustomUserDetailsService;
+import com.ejemplo.biblioteca.service.JwtService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ejemplo.biblioteca.dto.UsuarioCreateDTO;
-import com.ejemplo.biblioteca.dto.UsuarioDTO;
 import com.ejemplo.biblioteca.service.UsuarioService;
 
 import lombok.RequiredArgsConstructor;
@@ -19,9 +25,29 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
     
      private final UsuarioService usuarioService;
+     private final AuthenticationManager authenticationManager;
+     private final JwtService jwtService;
+     private final CustomUserDetailsService customUserDetailsService;
 
      @PostMapping("/registro")
      public ResponseEntity<UsuarioDTO> registrarUsuario( @RequestBody UsuarioCreateDTO dto){
         return new ResponseEntity<>(usuarioService.crear(dto), HttpStatus.CREATED);
-     } 
+     }
+
+    @PostMapping("/login")
+    @Operation(security = {})
+    public ResponseEntity<?> login(@RequestBody LoginDTO dto) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(dto.correo(), dto.password())
+            );
+            UserDetails user = customUserDetailsService.loadUserByUsername(dto.correo());
+            String token = jwtService.generarToken(user);
+            return ResponseEntity.ok(new AuthResponse(token));
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(401).body(
+                    new ApiError(401, "Credenciales incorrectas o token inválido")
+            );
+        }
+    }
 }
